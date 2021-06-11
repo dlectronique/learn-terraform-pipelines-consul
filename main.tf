133afd9e64b05805670b6942ba0bb80e7d93cc3f
D'lectronique [DevOpsDomi] <domi@dlectronique.com>
2021-06-11 10:05:00 +0100

    Update main.tf
    update workspace name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "dominique-t-consul"
     }
   }
   required_providers {
